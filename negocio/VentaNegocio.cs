7e6ea652b03d656818462dc4a968d4a04818ca59
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-01-04 20:27:06 -0300

    Inint Metodods para guardar archivos

---
+++ negocio/VentaNegocio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    internal class VentaNegocio
+    {
+        public List<Ventas> listar()
+        {
+            List<Ventas> listaVentas = new List<Ventas>();
+
+            return listaVentas;
+        }
+
+        public void agregar(Ventas venta)
+        {
+
+        }
+
+        public void eliminar(int id)
+        {
+
+        }
+
+        public List<Ventas> buscar(string filtro, int id, DateTime fecha)
+        {
+            List<Ventas> listaVentas = new List<Ventas>();
+
+            return listaVentas;
+        }
+    }
+}
