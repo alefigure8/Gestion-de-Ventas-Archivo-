370de2b1a645d765040421a351a903be46273e41
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-01-04 20:26:43 -0300

    Inint Ventas

---
+++ dominio/Ventas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Ventas
+    {
+        public int Id{ get; set; }
+        public DateTime  Fecha { get; set; }
+        public decimal Total { get; set; }
+        public bool Impuesto { get; set; }
+        public List<Venta> Venta { get; set; }
+
+        public Ventas()
+        {
+            Id = 0;
+            Fecha = DateTime.Now;
+            Total = 0;
+            Impuesto = false;
+            Venta = new List<Venta>();
+        }
+
+    }
+}
