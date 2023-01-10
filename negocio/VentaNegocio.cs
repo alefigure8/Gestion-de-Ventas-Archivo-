60d06108f47fe0e9011d974b1cbec1e4b516ec62
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-01-10 16:26:00 -0300

    Fixing método listar

--- negocio/VentaNegocio.cs
+++ negocio/VentaNegocio.cs
@@ -44,24 +44,27 @@ namespace negocio
 
                     foreach (var data in lines)
                     {
+                        int icolumn = 0;
+
                         //Lista Producto
                         ProductoNegocio productoNegocio = new ProductoNegocio();
                         List<Producto> listaProducto = new List<Producto>();
                         listaProducto = productoNegocio.listar();
 
-                        //Lsita Venta
+                        //Lsita Venta para colocar dentro de Ventas
                         List<Venta> listaVenta = new List<Venta>();
 
-                        int icolumn = 0;
+
                         //Ventas
                         Ventas aux = new Ventas();
+
                         aux.Id = int.Parse(data[icolumn++]);
                         aux.Fecha = DateTime.ParseExact(data[icolumn++], "dd/MM/yyyy", null);
                         aux.Total = Convert.ToDecimal(data[icolumn++].ToString().Replace(".", ","));
                         string aux_id_producto = data[icolumn++];
                         string aux_cantidad_venta = data[icolumn++];
 
-                        //Llave con Id de Productos
+                        //Llave con Id de Productos, cargamos uno por uno
                         List<string> id_Producto = new List<string>();
                         id_Producto = aux_id_producto.Split('.').ToList();
 
@@ -72,38 +75,48 @@ namespace negocio
                                 Venta aux_venta = new Venta();
                                 Producto aux_producto = listaProducto.Find(x => int.Parse(id) == x.Id);
 
-                                aux_venta.Id = aux_producto.Id;
-                                aux_venta.Codigo = aux_producto.Codigo;
-                                aux_venta.Nombre = aux_producto.Nombre;
-                                aux_venta.Descripcion = aux_producto.Descripcion;
-                                aux_venta.Precio = aux_producto.Precio;
-                                aux_venta.Stock = aux_producto.Stock;
+                               if(aux_venta != null)
+                                {
+                                    aux_venta.Id = aux_producto.Id;
+                                    aux_venta.Codigo = aux_producto.Codigo;
+                                    aux_venta.Nombre = aux_producto.Nombre;
+                                    aux_venta.Descripcion = aux_producto.Descripcion;
+                                    aux_venta.Precio = aux_producto.Precio;
+                                    aux_venta.Stock = aux_producto.Stock;
+                                    aux_venta.Costo = aux_producto.Costo;
 
-                                Marca marca = new Marca();
-                                marca.Id = aux_producto.MarcaInfo.Id;
-                                marca.Descripcion = aux_producto.MarcaInfo.Descripcion;
-                                aux_venta.MarcaInfo = marca;
 
-                                Categoria categoria = new Categoria();
-                                categoria.Id = aux_producto.CategoriaInfo.Id;
-                                categoria.Descripcion = aux_producto.CategoriaInfo.Descripcion;
-                                aux_venta.CategoriaInfo = categoria;
+                                    Marca marca = new Marca();
+                                    marca.Id = aux_producto.MarcaInfo.Id;
+                                    marca.Descripcion = aux_producto.MarcaInfo.Descripcion;
+                                    aux_venta.MarcaInfo = marca;
 
-                                listaVenta.Add(aux_venta);
+                                    Categoria categoria = new Categoria();
+                                    categoria.Id = aux_producto.CategoriaInfo.Id;
+                                    categoria.Descripcion = aux_producto.CategoriaInfo.Descripcion;
+                                    aux_venta.CategoriaInfo = categoria;
+
+                                    listaVenta.Add(aux_venta);
+                                }
                             }
                         }
 
-                        //Cargar cantidades de cada venta
+                        //Llave con cantidades de cada Venta
                         List<string> cantidad_venta = new List<string>();
                         cantidad_venta = aux_cantidad_venta.Split('.').ToList();
 
                         for (int i = 0; i < listaVenta.Count; i++)
                         {
-                            int aux_camtidad_vemta;
-                            int.TryParse(cantidad_venta[i], out aux_camtidad_vemta);
-                            listaVenta[i].Cantidad = aux_camtidad_vemta;
+                            int try_cantidad_venta;
+                            int.TryParse(cantidad_venta[i], out try_cantidad_venta);
+                            listaVenta[i].Cantidad = try_cantidad_venta;
+                            listaVenta[i].Total = listaVenta[i].Cantidad * listaVenta[i].Precio;
                         }
 
+                        //Agrega la lista de venta al atributo venta de aux (Ventas)
+                        aux.Venta = listaVenta;
+
+                        //Agregamos Ventas a la lista de ventas
                         listaVentas.Add(aux);
                     }
                 }          
@@ -122,8 +135,7 @@ namespace negocio
         {
             string path = Application.LocalUserAppDataPath + Opciones.Folder.DATABASE;
             string csv = string.Empty;
-            string id_venta = string.Empty;
-            string cantiadad_venta = string.Empty;
+
             List<Ventas> listaVentas = new List<Ventas>();
 
             //Si no existe el directorio, locrea
@@ -158,23 +170,25 @@ namespace negocio
                     //Agregamos la venta a la lista de venta
                     listaVentas.Add(venta);
 
-                    //Crear llave con Id de Venta
-                    foreach (var item in venta.Venta)
-                    {
-                        if(item.Id != 0)
-                            id_venta += item.Id.ToString() + ".";
-                    }
-
-                    //Crear llave con Id de Cantidades
-                    foreach (var item in venta.Venta)
-                    {
-                        if (item.Cantidad != 0)
-                            cantiadad_venta += item.Cantidad.ToString() + ".";
-                    }
-
                     //Adding the Rows
                     foreach (var item in listaVentas)
                     {
+                        string id_venta = string.Empty;
+                        string cantiadad_venta = string.Empty;
+
+                        foreach (var item2 in item.Venta)
+                        {
+                            if (item2.Id != 0)
+                                id_venta += item2.Id.ToString() + ".";
+                        }
+
+                        foreach (var item2 in item.Venta)
+                        {
+                            if (item2.Cantidad != 0)
+                                cantiadad_venta += item2.Cantidad.ToString() + ".";
+
+                        }
+
                         //Add the Data rows.
                         csv += $"{item.Id},{item.Fecha.ToString("dd/MM/yyyy")},{item.Total.ToString().Replace(".", ",")},{id_venta},{cantiadad_venta}";
                         //Add new line.
