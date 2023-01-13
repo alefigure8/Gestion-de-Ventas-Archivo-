c5ad6b79bba69d12ab4424c437ba9a475751477b
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-01-13 16:42:06 -0300

    Adding menu en ventana estadistica

--- presentación/frmEstadistica.cs
+++ presentación/frmEstadistica.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using negocio;
+using dominio;
 
 namespace presentación
 {
     public partial class frmEstadistica : Form
     {
-        public frmEstadistica()
+        private Form parent;
+
+        public frmEstadistica(Form parent)
         {
             InitializeComponent();
+            this.parent = parent;
+        }
+
+        private void frmEstadistica_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEstadisticaSemanal_Click(object sender, EventArgs e)
+        {
+            frmEstadisticaSemanales screen = new frmEstadisticaSemanales(parent);
+            screen.MdiParent = parent;
+            screen.Show();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, this.panelContainerOprions.ClientRectangle, Color.LightGray, ButtonBorderStyle.Solid);
         }
     }
 }
