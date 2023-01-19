650f7eee2c0543fd61cc355838fe28f3cb9e06c9
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-01-19 16:41:00 -0300

    Init Utils con Metodo para generar ID random

---
+++ configuracion/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace configuracion
+{
+    static public class Utils
+    {
+        public static int generarIdRandom()
+        {
+            int iSeed = 10;
+            Random ro = new Random(iSeed);
+            long tick = DateTime.Now.Ticks;
+            Random ran = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+            return ran.Next();
+        }
+    }
+}
