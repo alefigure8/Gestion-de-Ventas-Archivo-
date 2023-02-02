582b6f3280c5904bc1ea4b77f70cf64b45800360
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-02 15:21:14 -0300

    Add Constructor por defecto

--- dominio/Presupuesto.cs
+++ dominio/Presupuesto.cs
@@ -8,12 +8,24 @@ namespace dominio
 
         public Presupuesto()
         {
+            this.Id = 0;
             this.Codigo = "";
             this.Nombre = "";
             this.Precio = 0;
             this.Descripcion = "";
             this.cantidad = 0;
             this.total = 0;
+
+            Categoria auxCategoria = new Categoria();
+            auxCategoria.Id = 0;
+            auxCategoria.Descripcion = "";
+
+            Marca auxMarca= new Marca();
+            auxMarca.Id = 0;
+            auxMarca.Descripcion = "";
+
+            this.CategoriaInfo = auxCategoria;
+            this.MarcaInfo = auxMarca;
         }
     }
 }
--- dominio/Venta.cs
+++ dominio/Venta.cs
@@ -10,5 +10,29 @@ namespace dominio
     {
         public int Cantidad { get; set; }
         public decimal Total{ get; set; }
+
+        public Venta()
+        {
+            this.Id = 0;
+            this.Codigo = "";
+            this.Nombre = "";
+            this.Precio = 0;
+            this.Descripcion = "";
+            this.Cantidad = 0;
+            this.Total = 0;
+            this.Stock = 0;
+            this.Costo = 0;
+
+            Categoria auxCategoria = new Categoria();
+            auxCategoria.Id = 0;
+            auxCategoria.Descripcion = "";
+
+            Marca auxMarca = new Marca();
+            auxMarca.Id = 0;
+            auxMarca.Descripcion = "";
+
+            this.CategoriaInfo = auxCategoria;
+            this.MarcaInfo = auxMarca;
+        }
     }
 }
