0ce2540be983f4b12a415a4b47257a9004fc2715
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-03 16:57:33 -0300

    Delete campo ID

---
+++ presentación/frmAgregarLibre.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace presentación
+{
+    partial class frmAgregarLibre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelCargaProducto = new System.Windows.Forms.Panel();
+            this.lbErrorArticulo = new System.Windows.Forms.Label();
+            this.lbErrorDescripcion = new System.Windows.Forms.Label();
+            this.lbErrorPrecio = new System.Windows.Forms.Label();
+            this.txtAgregarPrecio = new System.Windows.Forms.TextBox();
+            this.txtAgregarDescripcion = new System.Windows.Forms.TextBox();
+            this.txtAgregarArticulo = new System.Windows.Forms.TextBox();
+            this.lbAgregarPrecio = new System.Windows.Forms.Label();
+            this.lbAgregarDescripcion = new System.Windows.Forms.Label();
+            this.lbAgregarArticulo = new System.Windows.Forms.Label();
+            this.lbTituloCargarProducto = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.panelCargaProducto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelCargaProducto
+            // 
+            this.panelCargaProducto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(62)))), ((int)(((byte)(79)))));
+            this.panelCargaProducto.Controls.Add(this.lbErrorArticulo);
+            this.panelCargaProducto.Controls.Add(this.lbErrorDescripcion);
+            this.panelCargaProducto.Controls.Add(this.lbErrorPrecio);
+            this.panelCargaProducto.Controls.Add(this.txtAgregarPrecio);
+            this.panelCargaProducto.Controls.Add(this.txtAgregarDescripcion);
+            this.panelCargaProducto.Controls.Add(this.txtAgregarArticulo);
+            this.panelCargaProducto.Controls.Add(this.lbAgregarPrecio);
+            this.panelCargaProducto.Controls.Add(this.lbAgregarDescripcion);
+            this.panelCargaProducto.Controls.Add(this.lbAgregarArticulo);
+            this.panelCargaProducto.Location = new System.Drawing.Point(33, 50);
+            this.panelCargaProducto.Name = "panelCargaProducto";
+            this.panelCargaProducto.Size = new System.Drawing.Size(371, 114);
+            this.panelCargaProducto.TabIndex = 1;
+            // 
+            // lbErrorArticulo
+            // 
+            this.lbErrorArticulo.AutoSize = true;
+            this.lbErrorArticulo.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbErrorArticulo.ForeColor = System.Drawing.Color.LightCoral;
+            this.lbErrorArticulo.Location = new System.Drawing.Point(323, 20);
+            this.lbErrorArticulo.Name = "lbErrorArticulo";
+            this.lbErrorArticulo.Size = new System.Drawing.Size(18, 18);
+            this.lbErrorArticulo.TabIndex = 17;
+            this.lbErrorArticulo.Text = "*";
+            // 
+            // lbErrorDescripcion
+            // 
+            this.lbErrorDescripcion.AutoSize = true;
+            this.lbErrorDescripcion.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbErrorDescripcion.ForeColor = System.Drawing.Color.LightCoral;
+            this.lbErrorDescripcion.Location = new System.Drawing.Point(323, 49);
+            this.lbErrorDescripcion.Name = "lbErrorDescripcion";
+            this.lbErrorDescripcion.Size = new System.Drawing.Size(18, 18);
+            this.lbErrorDescripcion.TabIndex = 16;
+            this.lbErrorDescripcion.Text = "*";
+            // 
+            // lbErrorPrecio
+            // 
+            this.lbErrorPrecio.AutoSize = true;
+            this.lbErrorPrecio.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbErrorPrecio.ForeColor = System.Drawing.Color.LightCoral;
+            this.lbErrorPrecio.Location = new System.Drawing.Point(211, 79);
+            this.lbErrorPrecio.Name = "lbErrorPrecio";
+            this.lbErrorPrecio.Size = new System.Drawing.Size(18, 18);
+            this.lbErrorPrecio.TabIndex = 14;
+            this.lbErrorPrecio.Text = "*";
+            // 
+            // txtAgregarPrecio
+            // 
+            this.txtAgregarPrecio.Location = new System.Drawing.Point(111, 76);
+            this.txtAgregarPrecio.Name = "txtAgregarPrecio";
+            this.txtAgregarPrecio.Size = new System.Drawing.Size(94, 20);
+            this.txtAgregarPrecio.TabIndex = 3;
+            // 
+            // txtAgregarDescripcion
+            // 
+            this.txtAgregarDescripcion.Location = new System.Drawing.Point(111, 47);
+            this.txtAgregarDescripcion.Name = "txtAgregarDescripcion";
+            this.txtAgregarDescripcion.Size = new System.Drawing.Size(205, 20);
+            this.txtAgregarDescripcion.TabIndex = 2;
+            // 
+            // txtAgregarArticulo
+            // 
+            this.txtAgregarArticulo.Location = new System.Drawing.Point(111, 18);
+            this.txtAgregarArticulo.Name = "txtAgregarArticulo";
+            this.txtAgregarArticulo.Size = new System.Drawing.Size(205, 20);
+            this.txtAgregarArticulo.TabIndex = 1;
+            // 
+            // lbAgregarPrecio
+            // 
+            this.lbAgregarPrecio.AutoSize = true;
+            this.lbAgregarPrecio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbAgregarPrecio.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgregarPrecio.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbAgregarPrecio.Location = new System.Drawing.Point(57, 77);
+            this.lbAgregarPrecio.Name = "lbAgregarPrecio";
+            this.lbAgregarPrecio.Size = new System.Drawing.Size(47, 16);
+            this.lbAgregarPrecio.TabIndex = 3;
+            this.lbAgregarPrecio.Text = "Precio";
+            // 
+            // lbAgregarDescripcion
+            // 
+            this.lbAgregarDescripcion.AutoSize = true;
+            this.lbAgregarDescripcion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbAgregarDescripcion.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgregarDescripcion.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbAgregarDescripcion.Location = new System.Drawing.Point(23, 48);
+            this.lbAgregarDescripcion.Name = "lbAgregarDescripcion";
+            this.lbAgregarDescripcion.Size = new System.Drawing.Size(81, 16);
+            this.lbAgregarDescripcion.TabIndex = 2;
+            this.lbAgregarDescripcion.Text = "Descripción";
+            // 
+            // lbAgregarArticulo
+            // 
+            this.lbAgregarArticulo.AutoSize = true;
+            this.lbAgregarArticulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbAgregarArticulo.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgregarArticulo.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbAgregarArticulo.Location = new System.Drawing.Point(46, 19);
+            this.lbAgregarArticulo.Name = "lbAgregarArticulo";
+            this.lbAgregarArticulo.Size = new System.Drawing.Size(58, 16);
+            this.lbAgregarArticulo.TabIndex = 1;
+            this.lbAgregarArticulo.Text = "Articulo";
+            // 
+            // lbTituloCargarProducto
+            // 
+            this.lbTituloCargarProducto.AutoSize = true;
+            this.lbTituloCargarProducto.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTituloCargarProducto.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbTituloCargarProducto.Location = new System.Drawing.Point(28, 22);
+            this.lbTituloCargarProducto.Name = "lbTituloCargarProducto";
+            this.lbTituloCargarProducto.Size = new System.Drawing.Size(296, 25);
+            this.lbTituloCargarProducto.TabIndex = 2;
+            this.lbTituloCargarProducto.Text = "CARGAR PRODUCTO LIBRE";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAceptar.Location = new System.Drawing.Point(33, 170);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 10;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Brown;
+            this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Tomato;
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkRed;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCancelar.Location = new System.Drawing.Point(114, 170);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // frmAgregarLibre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(86)))), ((int)(((byte)(111)))));
+            this.ClientSize = new System.Drawing.Size(436, 207);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.lbTituloCargarProducto);
+            this.Controls.Add(this.panelCargaProducto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmAgregarLibre";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Agregar Producto Libre";
+            this.Load += new System.EventHandler(this.frmAgregarLibre_Load);
+            this.panelCargaProducto.ResumeLayout(false);
+            this.panelCargaProducto.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCargaProducto;
+        private System.Windows.Forms.Label lbErrorArticulo;
+        private System.Windows.Forms.Label lbErrorDescripcion;
+        private System.Windows.Forms.Label lbErrorPrecio;
+        private System.Windows.Forms.TextBox txtAgregarPrecio;
+        private System.Windows.Forms.TextBox txtAgregarDescripcion;
+        private System.Windows.Forms.TextBox txtAgregarArticulo;
+        private System.Windows.Forms.Label lbAgregarPrecio;
+        private System.Windows.Forms.Label lbAgregarDescripcion;
+        private System.Windows.Forms.Label lbAgregarArticulo;
+        private System.Windows.Forms.Label lbTituloCargarProducto;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
